13282cc2f6bbeceb4a5e07e95821143481c8f804
SanjuMu <sanjusri.munigala@gmail.com>
2025-01-26 09:08:09 -0500

    project

---
+++ math_operations.py
@@ -0,0 +1,2 @@
+import math
+print('The Floor and Ceiling value of 23.56 are: '+ )
---
+++ project.py
@@ -0,0 +1,11 @@
+
+import math
+print("the Square root of 20 is",math.sqrt(4))
+
+import math
+print(math.copysign(-24,5))
+
+import math 
+print("The factorial of 9 is",math.factorial(9))
+
+
