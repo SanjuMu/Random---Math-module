28d5d11f226b0fda8ca34bfdf88333997bc14d6e
SanjuMu <sanjusri.munigala@gmail.com>
2025-01-26 10:34:41 -0500

    project

--- math_operations.py
+++
@@ -1,2 +0,0 @@
-import math
-print('The Floor and Ceiling value of 23.56 are: '+ )
--- project.py
+++ project.py
@@ -8,4 +8,16 @@ print(math.copysign(-24,5))
 import math 
 print("The factorial of 9 is",math.factorial(9))
 
+import math
+print(math.floor(6))
+
+import math
+print(math.isnan(5))
+
+import math
+print(math.degrees(30))
+
+import math
+print(math.ceil(25.78))
+
 
